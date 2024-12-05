3272f50e907dcc846c62742b048ba0c4f9a52a86
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2024-12-05 02:20:46 +0100

    Added an AtlyssButton class

---
+++ EasySettings/Elements/Button.cs
@@ -0,0 +1,35 @@
+﻿using Nessie.ATLYSS.EasySettings.Extensions;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace Nessie.ATLYSS.EasySettings.UIElements;
+
+public class AtlyssButton : BaseAtlyssElement
+{
+    public Text ButtonLabel;
+
+    private Button _button;
+
+    public Button Button
+    {
+        get => _button;
+        set
+        {
+            if (_button) _button.onClick.RemoveListener(Clicked);
+            _button = value;
+            if (_button) _button.onClick.RemoveListener(Clicked);
+        }
+    }
+
+    public UnityEvent OnClicked { get; } = new();
+
+    public void Initialize()
+    {
+        ButtonLabel.text = "Button";
+
+        Button.onClick.RemoveAndDisableAllListeners();
+        Button.onClick.AddListener(Clicked);
+    }
+
+    private void Clicked() => OnClicked.Invoke();
+}
