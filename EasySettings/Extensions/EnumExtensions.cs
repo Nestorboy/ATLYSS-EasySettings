eb7bfbbe76d445bf541bfbe5238be9812ff06241
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2025-05-02 20:29:30 +0200

    Add methods to work with enum indices

---
+++ EasySettings/Extensions/EnumExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Nessie.ATLYSS.EasySettings.Extensions;
+
+public static class EnumExtensions
+{
+    public static int GetIndex<T>(this T enumValue) where T : Enum
+    {
+        Type enumType = enumValue.GetType();
+        string[] enumNames = Enum.GetNames(enumType);
+        return Array.IndexOf(enumNames, Enum.GetName(enumType, enumValue));
+    }
+
+    public static T FromIndex<T>(int index) where T : Enum
+    {
+        Type enumType = typeof(T);
+        Array enumValues = Enum.GetValues(enumType);
+        return (T)(index >= 0 && index < enumValues.Length ? enumValues.GetValue(index) : -1);
+    }
+}
