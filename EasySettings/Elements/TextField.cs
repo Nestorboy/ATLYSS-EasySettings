715cb13fc2ec2fbf6088d787e14e8acce57cc388
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2026-02-14 16:31:55 +0100

    Use file scoped namespace in TextField

--- EasySettings/Elements/TextField.cs
+++ EasySettings/Elements/TextField.cs
@@ -1,53 +1,52 @@
 ﻿using UnityEngine.Events;
 using UnityEngine.UI;
 
-namespace Nessie.ATLYSS.EasySettings.UIElements
+namespace Nessie.ATLYSS.EasySettings.UIElements;
+
+public class AtlyssTextField : BaseAtlyssLabelElement, IValueElement
 {
-    public class AtlyssTextField : BaseAtlyssLabelElement, IValueElement
+    public InputField InputField
     {
-        public InputField InputField
+        get;
+        set
         {
-            get;
-            set
+            if (field != null) field.onEndEdit.RemoveListener(ValueChanged);
+
+            field = value;
+
+            if (field != null)
             {
-                if (field != null) field.onEndEdit.RemoveListener(ValueChanged);
-
-                field = value;
-
-                if (field != null)
-                {
-                    field.onEndEdit.AddListener(ValueChanged);
-                    field.text = AppliedValue;
-                }
+                field.onEndEdit.AddListener(ValueChanged);
+                field.text = AppliedValue;
             }
         }
-
-        public Text Placeholder;
-        public UnityEvent<string> OnValueChanged { get; } = new();
-
-        public string AppliedValue { get; private set; } = string.Empty;
-
-        public void Initialize()
-        {
-            LabelText = "Text Field";
-
-            if (InputField != null)
-            {
-                InputField.onEndEdit.RemoveListener(ValueChanged);
-                InputField.onEndEdit.AddListener(ValueChanged);
-                InputField.text = AppliedValue;
-            }
-        }
-
-        public void SetValue(string value)
-        {
-            if (InputField != null) InputField.text = value;
-        }
-
-        public void Apply() => AppliedValue = InputField.text;
-
-        public void Revert() => InputField.text = AppliedValue;
-
-        private void ValueChanged(string newValue) => OnValueChanged.Invoke(newValue);
     }
+
+    public Text Placeholder;
+    public UnityEvent<string> OnValueChanged { get; } = new();
+
+    public string AppliedValue { get; private set; } = string.Empty;
+
+    public void Initialize()
+    {
+        LabelText = "Text Field";
+
+        if (InputField != null)
+        {
+            InputField.onEndEdit.RemoveListener(ValueChanged);
+            InputField.onEndEdit.AddListener(ValueChanged);
+            InputField.text = AppliedValue;
+        }
+    }
+
+    public void SetValue(string value)
+    {
+        if (InputField != null) InputField.text = value;
+    }
+
+    public void Apply() => AppliedValue = InputField.text;
+
+    public void Revert() => InputField.text = AppliedValue;
+
+    private void ValueChanged(string newValue) => OnValueChanged.Invoke(newValue);
 }
